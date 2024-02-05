0f8b925009cd0083a8c4865b61329ad018b984f1
Wendy De luca <151148693+wendy090296@users.noreply.github.com>
2024-02-05 16:53:27 +0100

    first commit

--- src/Components/AllTheBooks.jsx
+++ src/Components/AllTheBooks.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Books from "./books/romance.json";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const AllTheBooks = () => {
   return (
@@ -10,19 +11,17 @@ const AllTheBooks = () => {
       <Row className="gap-4 justify-content-center">
         {Books.map((book) => {
           return (
-            <Card
-              style={{ width: "20rem" }}
-              className="col-12 col-md-6 col-lg-3"
-              key={book.asin}
-            >
-              <Card.Img variant="top" src={book.img} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>{book.category}</Card.Text>
-                <Card.Text>{book.price}$</Card.Text>
-                <Button variant="dark">Get it now!</Button>
-              </Card.Body>
-            </Card>
+            <Col>
+              <Card className="h-100" key={book.asin}>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.category}</Card.Text>
+                  <Card.Text>{book.price}$</Card.Text>
+                  <Button variant="dark">Get it now!</Button>
+                </Card.Body>
+              </Card>
+            </Col>
           );
         })}
       </Row>
--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,39 +1,102 @@
 import { Component } from "react";
 import SingleBook from "./SingleBook";
-import { Col, Form } from "react-bootstrap";
+import { Col, Form, Container, Row } from "react-bootstrap";
+// import CommentArea from "./CommentArea";
 
 class BookList extends Component {
   //questo componente riceve un json dalle props
 
   state = {
     searchValue: "", // stato INIZIALE
+    initialAsin: null, // valore di asin iniziale del libro selezionato
+    comments: [],
   };
+
+  // funzione che sovrascrive asin
+
+  changeAsin = (newAsin) => {
+    this.setState({
+      currentAsin: newAsin,
+    });
+  };
+
+  // fetch dei commenti utilizzando l'asin
+
+  fetchReviews() {
+    const { initialAsin } = this.state;
+    fetch(
+      `https://striveschool-api.herokuapp.com/api/comments/${initialAsin}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWJiYTBmYTViMjYxNTAwMTk4YTY5NTAiLCJpYXQiOjE3MDY3OTUyNTgsImV4cCI6MTcwODAwNDg1OH0.OS7BwSmuBObJTi9NSJxWK9rNr_h9PJfVgKz_DxNbgM0",
+        },
+      })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nella ricezione del server");
+        }
+      })
+      .then((reviews) => {
+        console.log("Nuovo array di feedback", reviews);
+        // setto nuovo stato con i commenti risultati dall'array
+        this.setState({
+          comments: reviews,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+
+  // gestisco aggiornamento del componente, verificando se l'asin é diverso da quello iniziale
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.initialAsin !== this.state.currentAsin) {
+      this.fetchComments();
+    }
+  }
 
   render() {
     return (
       <>
         <Form.Control
-          className="my-3"
+          className="my-3 "
           placeholder="Cerca i tuoi libri"
           value={this.state.searchValue}
           onChange={(e) => {
             this.setState({ searchValue: e.target.value });
           }}
         />
-        {this.props.jsonOfBooks
-          .filter((book) =>
-            book.title
-              .toLowerCase()
-              .includes(this.state.searchValue.toLowerCase())
-          )
-          .map((book) => (
-            <Col xs={12} md={6} lg={4} xl={3} key={book.asin}>
-              <SingleBook oneBook={book} />
+        <Container>
+          <Row>
+            <Col>
+              <Row>
+                {this.props.jsonOfBooks
+                  .filter((book) =>
+                    book.title
+                      .toLowerCase()
+                      .includes(this.state.searchValue.toLowerCase())
+                  )
+                  .map((book) => (
+                    <Col xs={12} md={6} lg={4} xl={3} key={book.asin}>
+                      <SingleBook oneBook={book} />
+                    </Col>
+                  ))}
+              </Row>
             </Col>
-          ))}
+            <Col xs={6}>
+              
+            {this.state.initialAsinAsin && <CommentArea asin={this.state.initialAsin} comments={this.state.comments}/>
+            // mostra i commenti se viene selezionato un libro
+            </Col>
+          }
+          </Row>
+        </Container>
       </>
-    );
-  }
-}
-
+    )
+    
+    
 export default BookList;
